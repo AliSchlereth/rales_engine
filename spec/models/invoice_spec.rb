8056ac863d0577f9d9d5c17494b9280ca16ede6a
Noah Berman <noah@noahberman.org>
2016-11-28 19:23:11 -0700

    invoice validations done

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	context "validations" do
+  it "is invalid without a status" do
+		merchant = create(:merchant)
+		customer = create(:customer)
+		invoice = Invoice.new(customer_id: 1, merchant_id: 1)
+
+		expect(invoice).to_not be_valid
+	 end
+		
+	it "is invalid	without a customer_id" do
+		merchant = create(:merchant)
+		customer = create(:customer)
+		invoice = Invoice.new(status: "success", merchant_id: 1)
+		
+		expect(invoice).to_not be_valid
+	end
+
+	it "is invalid without a merchant_id" do
+		merchant = create(:merchant)
+		customer = create(:customer)
+		invoice = Invoice.new(status: "success", customer_id:1)
+
+		expect(invoice).to_not be_valid
+	end	
+
+	it "is valid with all necessary info" do
+		merchant = create(:merchant)
+		customer = create(:customer)
+		invoice = Invoice.new(status: "success", merchant_id: merchant.id, customer_id: customer.id)
+
+		expect(invoice).to be_valid
+	 end
+	end
+
 end
