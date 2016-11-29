57c4522f1ea401f8ef12c350ef9c77b5f8f6dfce
Noah Berman <noah@noahberman.org>
2016-11-28 18:43:44 -0700

    complete database migrations

--- db/migrate/20161129013215_create_items.rb
+++ db/migrate/20161129013215_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-      t.text :name
-      t.text :description
+      t.text    :name
+      t.text    :description
       t.references :merchant, foreign_key: true
 
       t.timestamps null: false
