7299eed48c2ab734f80fee95aad44ff1f20aee8f
Noah Berman <noah@noahberman.org>
2016-11-29 13:22:21 -0700

    add find_all for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get '/merchants/find', to: "merchants/search#show"
       get '/merchants/find_all', to: "merchants/search#index"
       get '/invoices/find', to: "invoices/search#show"
+      get '/invoices/find_all', to: "invoices/search#index"
       resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
     end
