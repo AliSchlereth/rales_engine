4086efc15b13deb98ae273a833203064f57ce624
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-28 19:24:31 -0700

    Update item spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "is invalid without a credit_card_number" do
+      transaction = Transaction.new(result: "success", invoice_id: 1)
+      expect(transaction).to_not be_valid
+    end
+  end
+
 end
