2457e0a862d7d7eb523b8821d68ed4acdb504f54
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-28 19:28:31 -0700

    Update transaction spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Transaction, type: :model do
       transaction = Transaction.new(result: "success", invoice_id: 1)
       expect(transaction).to_not be_valid
     end
+
+    it "is invalid without a result" do
+      transaction = Transaction.new(credt_card_number: 123, invoice_id: 1)
+      expect(transaction).to_not be_valid
+    end
+
+    it "is valid with a result, credit_card_number, and invoice_id" do
+      invoice = create(:invoice)
+      transaction = Transaction.create(credt_card_number: 123, result: "success", invoice: invoice)
+      expect(transaction).to be_valid
+    end
+
   end
 
 end
