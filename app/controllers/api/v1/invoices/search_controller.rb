c6a3519d2c84451230c74b4460c4cb8ab89cf4d2
Noah Berman <noah@noahberman.org>
2016-11-29 12:09:44 -0700

    refactored search method

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::Invoices::SearchController < ApplicationController
   def show
-    search_area = params.keys.first
-    search_target = params.values.first
-    render json: Invoice.find_by("#{search_area}": "#{search_target}")
+    render json: Invoice.find_by(valid_search_parameters)
   end
+  
+  private
+  
+  def valid_search_parameters
+    params.permit(:status, :customer_id, :merchant_id, :created_at, :updated_at)
+  end
+  
   
 end
--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -26,7 +26,7 @@ describe "invoices endpoints" do
   end
   
   context 'GET /invoices/find' do
-    it "returns the right invoice" do
+    it "returns the right invoice for status" do
       invoice_1 = create(:invoice, status: "success")
       invoice_2 = create(:invoice, status: "fail")
     
@@ -36,6 +36,33 @@ describe "invoices endpoints" do
       expect(response).to be_success
       expect(result["status"]).to eq("fail")
       expect(result["id"]).to_not eq(invoice_1.id)
+    end
+    
+    it "returns the right invoice for customer_id" do
+      customer_1 = create(:customer)
+      customer_2 = create(:customer)
+      invoice_1 = create(:invoice, customer_id: "#{customer_1.id}")
+      invoice_2 = create(:invoice, customer_id: "#{customer_2.id}")
+      
+      get "/api/v1/invoices/find?customer_id=#{customer_1.id}"
+    
+      result = JSON.parse(response.body)
+      expect(response).to be_success
+      expect(result["customer_id"]).to eq("#{customer_1.id}".to_i)
+      expect(result["id"]).to_not eq(invoice_2.id)
+    end
+    
+    it "returns the right invoice for created_at" do
+      invoice_1 = create(:invoice)
+      invoice_2 = create(:invoice)
+      
+      get "/api/v1/invoices/find?created_at=#{invoice_1.created_at.strftime("%a, %-d %b %Y %H:%M:%S UTC +00:00")}"
+      result = JSON.parse(response.body)
+      binding.pry
+      
+      expect(response).to be_success
+      expect(result["created_at"]).to eq("#{invoice_1.created_at}")
+      expect(result["id"]).to_not eq(invoice_2.id)
     end
   end
 end
