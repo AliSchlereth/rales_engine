ecc9db605bc8d9ecf3ddfcede2cbdee6fd868c92
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-01 12:21:32 -0700

    Item updated at refactor

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -11,6 +11,9 @@ class Api::V1::Items::SearchController < ApplicationController
     elsif params[:created_at]
       item = Item.where(valid_search_parameters).order(:id).first
       render json: item
+    elsif params[:updated_at]
+      item = Item.where(valid_search_parameters).order(:id).first
+      render json: item
     else
       render json: Item.find_by(valid_search_parameters)
     end
