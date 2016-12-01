85063497f360dc3d521df513c924d07dfd5d0217
Noah Berman <noah@noahberman.org>
2016-12-01 13:46:00 -0700

    refactor invoice items search controller

--- app/controllers/api/v1/invoice_items/search_controller.rb
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -1,28 +1,11 @@
 class Api::V1::InvoiceItems::SearchController < ApplicationController
 
   def index
-    if params[:unit_price]
-      price = ((params[:unit_price].to_f) * 100).round()
-      render json: InvoiceItem.where(unit_price: price)
-    else
-      render json: InvoiceItem.where(valid_search_parameters)
-    end 
+    render json: InvoiceItem.find_all_invoice_items(params)
   end
 
   def show
-    if params[:unit_price]
-      price = ((params[:unit_price].to_f) * 100).round()
-      render json: InvoiceItem.find_by(unit_price: price)
-    else
-      render json: InvoiceItem.find_by(valid_search_parameters)
-    end
+    render json: InvoiceItem.find_invoice_item(params)
   end
-
-  private
-
-  def valid_search_parameters
-    params.permit(:id, :quantity, :unit_price, :item_id, :invoice_id, :created_at, :updated_at)
-  end
-
 
 end
