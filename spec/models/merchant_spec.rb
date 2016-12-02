a36905df598d9c21cd67263d78b2cda331b2c081
Ali Schlereth <aschlere@sbcglobal.net>
2016-12-01 20:07:55 -0700

    Single merchant revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -83,5 +83,36 @@ RSpec.describe Merchant, type: :model do
       expect(result.length).to eq(1)
       expect(result[0]).to be_a(Merchant)
     end
+
+    it "returns the total revenue for date x across all merchants in integer form" do
+      date = "2012-03-27 14:56:04"
+
+      merchant = Merchant.create(name: "Yay")
+      customer = Customer.create(first_name: "Hello", last_name: "Goodbye")
+      invoice = Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "shipped", created_at: date)
+      item = Item.create(merchant_id: merchant.id, name: "Coffee", description: "scrumptious", unit_price: 500)
+      invoice_item = InvoiceItem.create(item_id: item.id, invoice_id: invoice.id, quantity: 4, unit_price: 500)
+      transaction = Transaction.create(credit_card_number: 4815162342, result: "success", invoice_id: invoice.id)
+
+      merchant2 = Merchant.create(name: "Nooooo")
+      customer2 = Customer.create(first_name: "Hello", last_name: "Goodbye")
+      invoice2 = Invoice.create(customer_id: customer2.id, merchant_id: merchant2.id, status: "shipped", created_at: date)
+      item2 = Item.create(merchant_id: merchant2.id, name: "Coffee", description: "scrumptious", unit_price: 500)
+      invoice_item2 = InvoiceItem.create(item_id: item2.id, invoice_id: invoice2.id, quantity: 4, unit_price: 500)
+      transaction = Transaction.create(credit_card_number: 4815162342, result: "success", invoice_id: invoice2.id)
+
+      result = Merchant.revenue_by_date(date)
+      total_revenue = (invoice_item.quantity * invoice_item.unit_price) + (invoice_item2.quantity * invoice_item2.unit_price)
+
+      expect(result).to be_a(Integer)
+      expect(result).to eq(total_revenue)
+    end
+
+    it "returns the total revenue for that merchant across all transactions" do
+      result = @merchant1.single_merchant_revenue
+
+      expect(result).to be_a(Integer)
+      expect(result).to eq(1)
+    end
   end
 end
