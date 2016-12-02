8e4eb67773ef6ca7d7616c76768b4fa16e017cb1
Noah Berman <noah@noahberman.org>
2016-12-01 19:57:26 -0700

    merchant search re-refactored

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def index
-    render json: Merchant.find_all_merchants(params)
+    render json: Merchant.where(valid_search_parameters)
   end
   
   def show
-    render json: Merchant.find_merchant(params)
+    render json: Merchant.find_by(valid_search_parameters)
   end
+  
+  private
+  
+  def valid_search_parameters
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
+
   
 end
